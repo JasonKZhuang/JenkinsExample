9f8929c79ef5c286893573278597bc3690110361
Jason Zhuang <zhuangkaizhi@gmail.com>
2021-05-03 15:32:42 +1000

    test

--- src/main/java/com/jasonz/jenkins/controller/IndexController.java
+++ src/main/java/com/jasonz/jenkins/controller/IndexController.java
@@ -11,7 +11,7 @@ public class IndexController
 	@RequestMapping("/")
 	public String index()
 	{
-		return "Greetings from Spring Boot!" + new Date();
+		return "Greetings from Spring Boot!111" + new Date();
 	}
 
 }
